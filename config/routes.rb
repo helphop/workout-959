1a789730e899f8d56798d2b01448fd0a06c5b356
Mitchell Gould <mitchellgould7@gmail.com>
2017-03-25 12:34:10 +0700

    add all routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users
   get '/sign_up' => 'users#new', as: 'sign_up' 
 end
