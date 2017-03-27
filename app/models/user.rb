26fd7bf235421dbebc2c0a25f524a30e174fac67
Mitchell Gould <mitchellgould7@gmail.com>
2017-03-27 09:15:37 +0700

    change indentation to spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
 
-	acts_as_authentic
+  acts_as_authentic
 
-	validates :first_name, 
-  			presence: true,
-  			length: {minimum: 2}
+  validates :first_name, 
+        presence: true,
+        length: {minimum: 2}
 
   validates :last_name, 
-  			presence: true,
-  			length: {minimum: 2}
+        presence: true,
+        length: {minimum: 2}
 end
