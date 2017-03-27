994785273e343be916f881bc75713cfa162a3537
Mitchell Gould <mitchellgould7@gmail.com>
2017-03-27 09:21:40 +0700

    add migration to remove unused columns from users table

---
+++ db/migrate/20170327021735_remove_fields_from_users.rb
@@ -0,0 +1,30 @@
+class RemoveFieldsFromUsers < ActiveRecord::Migration[5.0]
+  def change
+    change_table(:users) do |t|
+      # Authlogic::ActsAsAuthentic::PersistenceToken
+      t.remove    :persistence_token
+      t.remove     :persistence_token
+
+      # Authlogic::ActsAsAuthentic::SingleAccessToken
+      t.remove    :single_access_token
+      t.remove    :single_access_token
+
+      # Authlogic::ActsAsAuthentic::PerishableToken
+      t.remove    :perishable_token
+      t.remove    :perishable_token
+
+      # Authlogic::Session::MagicColumns
+      t.remove   :login_count
+      t.remove   :failed_login_count
+      t.remove   :last_request_at
+      t.remove   :current_login_at
+      t.remove   :last_login_at
+      t.remove   :current_login_ip
+      t.remove   :last_login_ip
+
+      # Authlogic::Session::MagicStates
+      t.remove   :active
+      t.remove   :approved
+      t.remove   :confirmed
+  end
+end
