fde31e44b7b90f67292660f14541442fb66da7a7
Mitchell Gould <mitchellgould7@gmail.com>
2017-03-29 10:42:49 +0700

    remove password_salt field

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327023124) do
+ActiveRecord::Schema.define(version: 20170329030625) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
     t.string   "crypted_password"
-    t.string   "password_salt"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "persistence_token"
